57f51017572db7f84f1f85f835aee59b7e48fe23
Benjamin Mayne <BenjaminAdelaide@users.noreply.github.com>
2016-08-16 16:00:40 +0930

    Create plotCircos.R

---
+++ R/plotCircos.R
@@ -0,0 +1,49 @@
+#' plotCircos
+#'
+#' Plot a circos representing the cut site locations
+#'
+#' @param cutSites A matrix consisting of three columns, where the first column is the chromosome ID, the second and third column being the start and end position of the cut site respectively.
+#' @param genome A matrix with the lengths of the chromosomes. The first column is the chromosome ID and the second is the length of each chromosome.
+#' @param cutSite.colour The colour of the cut sites.
+#' @param genome.colour The colour of the chromosomes
+#' @usage plotCircos(cutSites, genome, cutSite.colour, genome.colour)
+#' @examples
+#' \dontrun{
+#' my_cuts <- matrix(c('chr1', 'chr2', 'chr3', 'chr4',
+#' '500', '1000', '750', '400',
+#' '500', '1000', '750', '400'), nrow=4, ncol=3)
+#' my_genome <- matrix(c('chr1', 'chr2', 'chr3', 'chr4',
+#'                    '1000', '2000','1500', '800'), nrow=4, ncol=3)
+#' plotCircos(cutSites = my_cuts, genome = my_genome, cutSite.colour = 'red', genome.colour = 'green')
+#'
+#' }
+#' @author Benjamin Mayne
+#' @export
+
+plotCircos <- function(cutSites, genome, cutSite.colour, genome.colour){
+
+  # Add another column into the genome matrix of 1s
+  genome <- cbind(genome, rep(1, nrow(genome)))
+  genome <- genome[ ,c(1,3,2)]
+
+  # Turn the data frames into GRanges objects
+  cutSites.gr <- GRanges(seqnames = Rle(values = c(cutSites[,1]), lengths = as.numeric(rep('1', nrow(cutSites)))),
+                         ranges = IRanges(start = as.numeric(cutSites[,2]), end = as.numeric(cutSites[,3])))
+  genome.gr <- GRanges(seqnames = Rle(values = c(genome[,1]), lengths = as.numeric(rep('1', nrow(genome)))),
+                         ranges = IRanges(start = as.numeric(genome[,2]), end = as.numeric(genome[,3])))
+
+  #seqnames(cutSites.gr) <- seqnames(genome.gr)
+  seqlevels(genome.gr) <- as.character(genome[,1])
+  seqlengths(genome.gr) <- as.integer(genome[,3])
+
+  seqlevels(cutSites.gr, force = TRUE) = seqlevels(genome.gr)
+  seqlengths(cutSites.gr)=as.integer(genome[,3])
+
+  # Plot using specified colours
+  p <- ggbio() + circle(cutSites.gr, geom = "rect", color = cutSite.colour) +
+    circle(genome.gr, geom = "ideo", fill = genome.colour) +
+    circle(genome.gr, geom = "scale", size = 2) +
+    circle(genome.gr, geom = "text", aes(label = seqnames), vjust = 0, size = 3)
+  p
+
+}
