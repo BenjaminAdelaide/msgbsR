2df4bc62b0248fcdb81a3fdb7fbe3620e510e100
Benjamin Mayne <BenjaminAdelaide@users.noreply.github.com>
2016-11-22 21:50:19 +1030

    Add files via upload

--- R/plotCounts.R
+++ R/plotCounts.R
@@ -19,8 +19,6 @@
 
 
 plotCounts <- function(countMatrix, condition1, condition2 = NULL){
-  requireNamespace('ggplot2', quietly = TRUE)
-
   # Unit tests
   ## Check if the countMatrix is a matrix
   countMatrixClass <- class(countMatrix)[1]
